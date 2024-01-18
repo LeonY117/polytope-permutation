ba25742461565b7517d06505dbd375fa86173ba7
Leon Yao <leon.yao117@gmail.com>
2024-01-18 13:33:33 +0000

    refactor puzzle loading

--- src/mechanism/utils.py
+++ src/mechanism/utils.py
@@ -1,4 +1,10 @@
-from typing import List, Any
+import json
+
+import pandas as pd
+
+from typing import Dict, List, Any
+
+from permute import reverse_perm, perm_to_swap
 
 
 def diff_between_states(state1: List[Any], state2: List[Any]) -> int:
@@ -30,3 +36,39 @@ def get_inverse_move(move_name: str) -> str:
         return move_name[1:]
     else:
         return f"-{move_name}"
+
+
+def load_puzzle(
+    puzzle_name: str, convert_to_swaps=True, puzzle_dir="../../puzzles"
+) -> (Dict[str, List[int]], int):
+    """Retrieves and returns the moves and final position of the puzzle"""
+    # load the moves:
+    with open(f"{puzzle_dir}/{puzzle_name}/moves.json") as f:
+        moves = json.load(f)
+
+    # add reversed moves
+    reversed_moves = {}
+    for move_name, perm in moves.items():
+        reversed_perm = reverse_perm(perm)
+        if reversed_perm == perm:
+            continue
+        reversed_moves[f"-{move_name}"] = reversed_perm
+
+    moves.update(reversed_moves)
+
+    if convert_to_swaps:
+        for move_name, perm in moves.items():
+            moves[move_name] = perm_to_swap(perm)
+
+    # get final position (from the first puzzle)
+    df = pd.read_csv(f"puzzles/{puzzle_name}/puzzles.csv")
+    state = df.iloc[0].to_numpy()[3].split(";")
+    c = -1
+    mapping = {}
+    for i, s in enumerate(state):
+        if s not in mapping.keys():
+            c += 1
+            mapping[s] = c
+        state[i] = mapping[s]
+
+    return moves, state
